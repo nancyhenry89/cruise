75013755f9a26c9a64737f61be0f15e694355f24
nancyhenry89 <nancyhenry89@gmail.com>
2018-09-06 12:37:14 +0200

    Members page mobile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+    $('.popup-title').click(function() {
+        $(this).siblings('.popup-cont').fadeIn();
+    });
+    $('.close').click(function() {
+        $(this).parents('.popup-cont').fadeOut();
+    });
+
 $('.free').click(function(){
     $('#thankYou').show();
     $('.content').hide();
